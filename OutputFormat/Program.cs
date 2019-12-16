637bf7619fae3409b10be1dda8d66e9f8e814791
Jesure <51762237+Jesure@users.noreply.github.com>
2019-12-16 22:42:00 +0500

    Add files via upload

---
+++ OutputFormat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace OutputFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = args[0].Length;
+            
+            for (int i=args[0].Length;i>0;i--)
+            {
+                s--;
+                if (args[0].Substring(s, 1) == ".")
+                {
+                    Console.WriteLine(args[0].Substring(s,(args[0].Length-s)));
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
